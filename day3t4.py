3c000a17a42a2854ef0453e426a8c247f70c6efa
afnaaakp <sherinafna777@gmail.com>
2026-06-10 12:40:37 +0530

    Add files via upload

---
+++ day3t4.py
@@ -0,0 +1,6 @@
+list1=["shadhi","nidhu","afi"]
+list2=["shibi","shai","jasi","nafi"]
+merged_list=list1+list2
+unique_list=list(set(merged_list))
+unique_list.sort()
+print("the sorted list is:",unique_list)
